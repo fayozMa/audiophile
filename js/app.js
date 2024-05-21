331e369f28b8499a3bec410f48d1e0a90ce3dad8
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-05-21 22:49:57 +0500

    [FIX] some bugs

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 //Elements
 const cartBtn = document.getElementById("cart");
 const modal = document.getElementById("modal");
-//functions
+//Functions
 const addHidden = () => {
   modal.classList.add("hidden");
-  overlay.classList.add("hidden");
 };
+const modalTogle = () => {
+  if (modal.classList.contains("hidden")) {
+    modal.classList.remove("hidden");
+  } else modal.classList.add("hidden");
+}
 //Events
-cartBtn.addEventListener("click", () => {
-  if(modal.classList.contains("hidden")){
-    modal.classList.remove("hidden")
-  }
-  else(
-    modal.classList.add("hidden")
-  )
-});
+cartBtn.addEventListener("click", modalTogle );
 document.addEventListener("keydown", (e) => {
   if (e.key == "Escape") {
     addHidden();
